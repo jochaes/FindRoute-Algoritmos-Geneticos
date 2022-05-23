282383e8aedc5eaf1802822ee47259929436bfe4
Josué Chaves Araya <d4n11083@estudiantec.cr>
2022-05-22 20:15:36 -0600

    Docu para clase que dibuja el individuo

--- js/individuo_dibujo.js
+++ js/individuo_dibujo.js
@@ -1,4 +1,16 @@
-
+/**
+    @class dibujoIndividuo
+    @classdesc Se encarga de dibujar los individuos en la interfaz 
+    @param {string} id_individuo    El identificador del Individuo, para buscarlo en el HTML
+    @param {int}    pCuadro_width   Ancho de los cuadritos en dónde va a vivir el SVG del individuo
+    @param {int}    pCuadro_height  Alto de los cuadritos en dónde va a vivir el SVG del individuo
+    @param {string} pVentana        El elemento HTML en donde se van a dibujar los individuos
+    @param {string} pSVG_NS         "Libreria" de los SVG
+    @param {int}    init_row        Indice de la fila inicial
+    @param {int}    init_column     Indice de la columna inicila 
+    @param {string} pMovimientos    String con los movimientos del individuo en la generación actual
+    
+ */
 class dibujoIndividuo {
 
 
@@ -10,30 +22,29 @@ class dibujoIndividuo {
 
         this.ventana = pVentana
         
-        this.cx = Math.floor(this.s_w/2) + (init_column) * this.s_w
+        this.cx = Math.floor(this.s_w/2) + (init_column) * this.s_w   // Calcula la mitad del cuadrito, y le suma los espacios para pintarlo en la columna correspondiente 
 
-        this.cy = Math.floor(this.s_h/2) + (init_row) * this.s_h
+        this.cy = Math.floor(this.s_h/2) + (init_row) * this.s_h       // Calcula la mitad del alto del  cuadrito, y le suma los espacios para pintarlo en la fila correspondiente 
 
         this.r  = Math.min( Math.floor(this.s_w/2), Math.floor(this.s_h/2) ) -  Math.floor((this.s_w/5)) //Calcular el radio de la bolita
+        
         this.stroke = "green"
         this.fill = "yellow"
 
         this.SVG_NS = pSVG_NS
-
         this.movimientos = pMovimientos
 
-        this.drawCircle()
+        this.drawCircle()       // Pinta el circulo 
     }
 
     /**
-     * Dibuja los circulos 
-     * @param {Array}  objet: Atributos de los bicho
-     * @param {SVG}  ventana: Es el tablero de juego 
-     * @param {String}    id: Es es el identificador del bicho
+     * Dibuja los circulos con la información del constructor
     */
     drawCircle = () => {
-        let elementFather = document.getElementById(this.ventana);
-        let circle = document.createElementNS(this.SVG_NS, "circle");
+        let elementFather = document.getElementById(this.ventana);      //Encuentra el elemento en dónde va a dibujar el circulo
+        let circle = document.createElementNS(this.SVG_NS, "circle");   //Crea el SVG del  Circulo
+        
+        //Coloca los Atributos al elemento
         circle.setAttributeNS(null, "id", this.id);
 
         circle.setAttributeNS(null, "cx", this.cx);
@@ -51,16 +62,13 @@ class dibujoIndividuo {
         //     }
         // }
 
-        elementFather.appendChild(circle);
-        return circle;
+        elementFather.appendChild(circle);                              //Introduce el circulo al SVG 
+        //return circle;
     }
 
 
     /**
-     * Dibuja los circulos 
-     * @param {Array}  objet: Atributos de los bicho
-     * @param {SVG}  ventana: Es el tablero de juego 
-     * @param {String}    id: Es es el identificador del bicho
+     * Borra el circulo del SVG
     */
     erase = () => {
         let element = document.getElementById(this.id);
